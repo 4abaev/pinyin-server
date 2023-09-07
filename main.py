61610111f18d79ee4912438b0ae408f527894fbf
4abaev <progchab@gmail.com>
2023-09-07 17:31:10 +0300

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from pypinyin import pinyin
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],  # Разрешенные источники (указывайте адрес вашего фронтенда)
+    allow_methods=['*'],  # Разрешенные HTTP-методы (в данном случае все)
+    allow_headers=['*'],  # Разрешенные HTTP-заголовки (в данном случае все)
+    allow_credentials=True,  # Разрешить отправку куки и заголовка авторизации
+    expose_headers=['Content-Disposition'],  # Заголовки, которые могут быть доступны клиенту
+)
+
+class Body(BaseModel):
+    text: str
+
+@app.post('/get_pinyin')
+def get_pinyin(body: Body):
+    # Генерируем Pinyin с акцентами (тонами)
+    pinyin_result = pinyin(body.text)
+
+    return pinyin_result
